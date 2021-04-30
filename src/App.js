a5a4202ab3a0289b2cacd7ab41cc3b4657680f6f
Emmanuel Ekpobimi <noble4special247@gmail.com>
2021-04-30 13:18:14 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <p>
+          <em>
+            <strong>Welcome to my RECIPE APP Tutorial</strong>
+          </em>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
