7d08b78dfc5a0c0fc6ef4013f622c7043896593f
Emmanuel Ekpobimi <noble4special247@gmail.com>
2021-05-17 05:01:59 +0100

    Modified the RecipeList Component (changed map key from name to id, h1 to h2)

--- src/components/pages/RecipeList.js
+++ src/components/pages/RecipeList.js
@@ -9,12 +9,12 @@ class RecipeList extends Component {
         <div className='container py-3'>
           {/* Title */}
           <div className='col-10 mx-auto col-md-6 text-center text-uppercase mb-3'>
-            <h1 className='text-slanted'>Recipe List</h1>
+            <h2 className='text-slanted'>Recipe List</h2>
           </div>
           {/* End of title */}
           <div className='row'>
             {recipes.map((recipe) => (
-              <SingleRecipe key={recipe.name} recipe={recipe} />
+              <SingleRecipe key={recipe.id} recipe={recipe} />
             ))}
           </div>
         </div>
