ab3e518d6ef2e1f409cd298fab683046542dfb5f
Emmanuel Ekpobimi <noble4special247@gmail.com>
2021-05-17 11:06:35 +0100

    Modified Axios request (removed async await, and replaced it with .then and catch methods), Added loading and error states

--- src/components/pages/Recipes.js
+++ src/components/pages/Recipes.js
@@ -11,7 +11,7 @@ class Recipes extends Component {
       recipes: [],
       errorMessage: "",
       search: "",
-      loading: false,
+      loading: true,
     };
   }
 
@@ -26,29 +26,57 @@ class Recipes extends Component {
   };
 
   async componentDidMount() {
-    this.setState({ loading: true });
-
-    const response = await axios.get("https://a.nacapi.com/recipes");
-    console.log(response);
-
-    this.setState({ recipes: response.data, loading: false });
+    // this.setState({ loading: true });
+    axios
+      .get("https://a.nacapi.com/recipes")
+      .then((response) => {
+        console.log(response);
+        this.setState({ recipes: response.data, loading: false });
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({
+          loading: false,
+          errorMessage: error.message,
+        });
+      });
   }
 
   render() {
     return (
-      <div className='container'>
-        <small>Hello from recipes page</small>
-        <Search
-          search={this.state.search}
-          handleChange={this.handleChange}
-          handleSubmit={this.handleSubmit}
-        />
-        <RecipeList
-          recipes={this.state.recipes}
-          errorMessage={this.state.errorMessage}
-          loading={this.state.loading}
-        />
-      </div>
+      <React.Fragment>
+        <div className='container'>
+          {/* <small>Hello from recipes page</small> */}
+          <Search
+            search={this.state.search}
+            handleChange={this.handleChange}
+            handleSubmit={this.handleSubmit}
+          />
+        </div>
+        {this.state.loading && (
+          <div className='container my-5'>
+            <h2 className='text-orange text-center text-uppercase'>
+              Loading......
+            </h2>
+          </div>
+        )}
+        {this.state.recipes && (
+          <div className='container my-4'>
+            <RecipeList
+              recipes={this.state.recipes}
+              errorMessage={this.state.errorMessage}
+              loading={this.state.loading}
+            />
+          </div>
+        )}
+        {this.state.errorMessage && (
+          <div className='container my-5'>
+            <h2 className='text-danger text-center text-uppercase'>
+              {this.state.errorMessage}
+            </h2>
+          </div>
+        )}
+      </React.Fragment>
     );
   }
 }
