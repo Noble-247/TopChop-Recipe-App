1eba4234dff8da78a8b15dc63663bc99a701dd09
Emmanuel Ekpobimi <noble4special247@gmail.com>
2021-06-01 12:25:11 +0100

    Replace class declaration with arrow function

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -1,25 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Footer extends Component {
-  render() {
-    return (
-      <div className='container-fluid bg-dark py-3 text-white text-center'>
-        <small>
-          &copy; TopChops Solutions 2021
-          <br />
-          Designed by {""}
-          <a
-            href='https://noble-247.github.io/Emmanuel'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='text-orange'
-          >
-            Emmanuel
-          </a>
-        </small>
-      </div>
-    );
-  }
-}
+const Footer = () => {
+  return (
+    <div className='container-fluid bg-dark py-3 text-white text-center'>
+      <small>
+        &copy; TopChops Solutions 2021
+        <br />
+        Designed by {""}
+        <a
+          href='https://noble-247.github.io/Emmanuel'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='text-orange'
+        >
+          Emmanuel
+        </a>
+      </small>
+    </div>
+  );
+};
 
 export default Footer;
