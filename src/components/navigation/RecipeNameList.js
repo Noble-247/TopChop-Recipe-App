d8759eb884178873064acdfea4993189e1bbcffa
Emmanuel Ekpobimi <noble4special247@gmail.com>
2021-06-01 12:37:09 +0100

    Replace class declaration with arrow function

--- src/components/navigation/RecipeNameList.js
+++ src/components/navigation/RecipeNameList.js
@@ -1,23 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import SingleRecipeName from "./SingleRecipeName";
 import PropTypes from "prop-types";
 
-class RecipeNameList extends Component {
-  static propTypes = {
-    recipeNames: PropTypes.array.isRequired,
-  };
+const RecipeNameList = ({ recipeNames }) => {
+  return (
+    <ul className='list-group list-group-flush bg-dark text-dark'>
+      <h4 className='text-slanted text-white'>Popular Foods</h4>
+      {recipeNames.map((recipeName) => (
+        <SingleRecipeName key={recipeName.id} recipeName={recipeName} />
+      ))}
+    </ul>
+  );
+};
 
-  render() {
-    const { recipeNames } = this.props;
-    return (
-      <ul className='list-group list-group-flush bg-dark text-dark'>
-        <h4 className='text-slanted text-white'>Popular Foods</h4>
-        {recipeNames.map((recipeName) => (
-          <SingleRecipeName key={recipeName.id} recipeName={recipeName} />
-        ))}
-      </ul>
-    );
-  }
-}
+RecipeNameList.propTypes = {
+  recipeNames: PropTypes.array.isRequired,
+};
 
 export default RecipeNameList;
