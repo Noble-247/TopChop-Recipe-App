7705c845a41a5f99af39a3dbc45d4d26b298fb84
Emmanuel Ekpobimi <noble4special247@gmail.com>
2021-06-01 12:50:03 +0100

    Replace class declaration with arrow function

--- src/components/utilities_components/Search.js
+++ src/components/utilities_components/Search.js
@@ -1,68 +1,65 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
-class Search extends Component {
-  static propTypes = {
-    handleChange: PropTypes.func.isRequired,
-    handleSubmit: PropTypes.func.isRequired,
-    fetchRecipes: PropTypes.func.isRequired,
-    search: PropTypes.string.isRequired,
-  };
-
-  render() {
-    const { handleChange, handleSubmit, fetchRecipes, search } = this.props;
-    return (
-      <div className='container'>
-        <div className='row'>
-          <div className='col-10 mx-auto col-md-8 mt-5 text-center'>
-            <h1 className='text-slanted text-capitalize'>
-              Search Recipes with
-              <strong className='text-orange'>TopChops </strong>
-            </h1>
-            <form className='mt-4'>
-              <label htmlFor='search' className='text-capitalize'>
-                <strong>
-                  Enter the recipe name to get a target recipe <br />
-                  Click back to recipes list to return to the main recipes list
-                </strong>
-              </label>
-              <div className='input-group'>
-                <input
-                  type='text'
-                  name='search'
-                  id='search'
-                  className='form-control'
-                  placeholder='Enter the recipe name eg Strawberry Toast'
-                  value={search}
-                  onChange={handleChange}
-                />
-                <div className='input-group-append'>
-                  <button
-                    type='submit'
-                    className='bg-orange text-white btn'
-                    onClick={handleSubmit}
-                  >
-                    <i className='fas fa-search'></i>
-                  </button>
-                </div>
-              </div>
-              <div>
-                <Link
-                  to='/recipes'
-                  className='btn bg-dark btn-block text-white text-capitalize'
-                  onClick={fetchRecipes}
+const Search = ({ handleChange, handleSubmit, fetchRecipes, search }) => {
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col-10 mx-auto col-md-8 mt-5 text-center'>
+          <h1 className='text-slanted text-capitalize'>
+            Search Recipes with
+            <strong className='text-orange'>TopChops </strong>
+          </h1>
+          <form className='mt-4'>
+            <label htmlFor='search' className='text-capitalize'>
+              <strong>
+                Enter the recipe name to get a target recipe <br />
+                Click back to recipes list to return to the main recipes list
+              </strong>
+            </label>
+            <div className='input-group'>
+              <input
+                type='text'
+                name='search'
+                id='search'
+                className='form-control'
+                placeholder='Enter the recipe name eg Strawberry Toast'
+                value={search}
+                onChange={handleChange}
+              />
+              <div className='input-group-append'>
+                <button
+                  type='submit'
+                  className='bg-orange text-white btn'
+                  onClick={handleSubmit}
                 >
-                  Click here to return to the recipes list after viewing search
-                  results
-                </Link>
+                  <i className='fas fa-search'></i>
+                </button>
               </div>
-            </form>
-          </div>
+            </div>
+            <div>
+              <Link
+                to='/recipes'
+                className='btn bg-dark btn-block text-white text-capitalize'
+                onClick={fetchRecipes}
+              >
+                Click here to return to the recipes list after viewing search
+                results
+              </Link>
+            </div>
+          </form>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+Search.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  fetchRecipes: PropTypes.func.isRequired,
+  search: PropTypes.string.isRequired,
+};
 
 export default Search;
