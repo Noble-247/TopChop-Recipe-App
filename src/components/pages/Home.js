67289d40cdb4dfd40927715c6a1369721f4ab010
Emmanuel Ekpobimi <noble4special247@gmail.com>
2021-06-01 12:43:31 +0100

    Replace class declaration with arrow function

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,19 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import Banner from "../navigation/Banner";
 import { Link } from "react-router-dom";
 
-class Home extends Component {
-  render() {
-    return (
-      <div className='container-fluid'>
-        <Banner title='Amazing Recipes'>
-          <Link to='recipes' className='btn btn-secondary btn-lg mt-3'>
-            Search Recipes
-          </Link>
-        </Banner>
-      </div>
-    );
-  }
-}
+const Home = () => {
+  return (
+    <div className='container-fluid'>
+      <Banner title='Amazing Recipes'>
+        <Link to='recipes' className='btn btn-secondary btn-lg mt-3'>
+          Search Recipes
+        </Link>
+      </Banner>
+    </div>
+  );
+};
 
 export default Home;
