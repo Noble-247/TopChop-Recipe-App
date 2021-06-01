aaeac37bcf2ea8cda6bc52b6adbafcdc62286d93
Emmanuel Ekpobimi <noble4special247@gmail.com>
2021-06-01 12:44:34 +0100

    Replace class declaration with arrow function

--- src/components/pages/RecipeList.js
+++ src/components/pages/RecipeList.js
@@ -1,26 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import SingleRecipe from "./SingleRecipe";
 
-class RecipeList extends Component {
-  render() {
-    const { recipes } = this.props;
-    return (
-      <React.Fragment>
-        <div className='container py-3'>
-          {/* Title */}
-          <div className='col-10 mx-auto col-md-6 text-center text-uppercase mb-3'>
-            <h2 className='text-slanted'>Recipe List</h2>
-          </div>
-          {/* End of title */}
-          <div className='row'>
-            {recipes.map((recipe) => (
-              <SingleRecipe key={recipe.id} recipe={recipe} />
-            ))}
-          </div>
+const RecipeList = ({ recipes }) => {
+  return (
+    <React.Fragment>
+      <div className='container py-3'>
+        {/* Title */}
+        <div className='col-10 mx-auto col-md-6 text-center text-uppercase mb-3'>
+          <h2 className='text-slanted'>Recipe List</h2>
         </div>
-      </React.Fragment>
-    );
-  }
-}
+        {/* End of title */}
+        <div className='row'>
+          {recipes.map((recipe) => (
+            <SingleRecipe key={recipe.id} recipe={recipe} />
+          ))}
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default RecipeList;
