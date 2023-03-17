0e15defa2f36fa49d19aaa6e17927212cba4fc44
Emmanuel Ekpobimi <noble4special247@gmail.com>
2023-03-17 14:19:55 +0100

    Convert from class to functional component, and Implement the SiteUnderConstruction logic in the App component.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,55 @@
-import React, { Component } from "react";
+import { useEffect, useState } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import "./index.css";
 import Home from "./components/pages/Home";
 import Recipes from "./components/pages/Recipes";
 import SingleRecipesDetails from "./components/pages/SingleRecipesDetails";
 import ErrorPage from "./components/pages/ErrorPage";
 import NavBar from "./components/navigation/NavBar";
 import Footer from "./components/navigation/Footer";
+import SiteUnderConstruction from "./components/pages/SiteUnderConstruction";
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <main>
-          {/* Navbar */}
-          <NavBar />
-          <Switch>
-            <Route path='/' exact component={Home} />
-            <Route path='/recipes' exact component={Recipes} />
-            <Route path='/recipes/:id' component={SingleRecipesDetails} />
-            <Route path='*' component={ErrorPage} />
-            <Route path='/recipes/*' component={ErrorPage} />
-          </Switch>
-          <Footer />
-        </main>
-      </Router>
-    );
+function App() {
+  const [mainContent, setMainContent] = useState(false);
+  const [isUnderConstruction, setIsUnderConstruction] = useState(true);
+
+  useEffect(() => {
+    setIsUnderConstruction(true);
+    setMainContent(false);
+  }, []);
+
+  function showMainContent() {
+    setMainContent(true);
+    setIsUnderConstruction(false);
   }
+
+  return (
+    <Router>
+      {isUnderConstruction && (
+        <section>
+          <SiteUnderConstruction showMainContent={showMainContent} />
+        </section>
+      )}
+      {mainContent && (
+        <section>
+          <NavBar />
+          <main>
+            <Switch>
+              <Route path='/' exact component={Home} />
+              <Route path='/recipes' exact component={Recipes} />
+              <Route path='/recipes/:id' component={SingleRecipesDetails} />
+              <Route path='*' component={ErrorPage} />
+              <Route path='/recipes/*' component={ErrorPage} />
+            </Switch>
+          </main>
+          <footer>
+            <Footer />
+          </footer>
+        </section>
+      )}
+    </Router>
+  );
 }
 
 export default App;
